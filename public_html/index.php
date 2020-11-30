6afcf856bc3b4d297573d974b98cd350f41626af
Esteban Murillo <71365741+Estebanjm1@users.noreply.github.com>
2020-11-30 15:07:30 -0600

    Prueba de commit

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,7 @@
 
 <html>
     <head>
-        <title>TRAVELING EARTH</title>
+        <title>TRAVELING EARTH -  Esto es una prueba</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="stylesheet" href="CSS/main.css" />
